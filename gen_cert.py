7265d9f7e31585286303070442a6e7465dc1bcc1
root <root@ip-10-0-0-71.ec2.internal>
2015-04-07 11:04:55 -0400

    Testing PR for certificates

--- gen_cert.py
+++ gen_cert.py
@@ -37,6 +37,8 @@ from bidi.algorithm import get_display
 import arabic_reshaper
 
 from opaque_keys.edx.keys import CourseKey
+from opaque_keys import InvalidKeyError
+from opaque_keys.edx.locations import SlashSeparatedCourseKey
 
 reportlab.rl_config.warnOnMissingFontGlyphs = 0
 
